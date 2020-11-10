b772dbee1ca1856b5f08ac2993ebbdebb3176f4b
Nelson Foltz <nelsfoltz@gmail.com>
2020-11-10 16:43:54 -0500

    add: display single page

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,13 @@ import { useFetch } from './useFetch';
 import Follower from './Follower';
 function App() {
   const { loading, data } = useFetch();
-  console.log(loading, data);
+  const [page, setPage] = useState(0);
+  const [followers, setFollowers] = useState([]);
+
+  useEffect(() => {
+    if (loading) return;
+    setFollowers(data[page]);
+  }, [loading]);
 
   return (
     <main>
@@ -13,7 +19,7 @@ function App() {
       </div>
       <section className="followers">
         <div className="container">
-          {data.map(follower => {
+          {followers.map(follower => {
             return <Follower key={follower.id} {...follower} />;
           })}
         </div>
